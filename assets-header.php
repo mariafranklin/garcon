1ff19b09aebd471e42e88ea7b4488d0aaedd9491
“[maria <[franklin@kambaa.com]>
2018-07-11 20:14:47 +0530

    alter

---
+++ assets-header.php
@@ -0,0 +1,5 @@
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Rancho" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700,700i" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
---
+++ index.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<?php include 'assets-header.php'; ?>
+
+</head>
+<body>
+<section class="hero">
+
+<header>
+	<?php include 'menu.php'; ?>
+</header>
+
+
+<div class="container">
+<img src="images/slider-icon.png" class="main-img"/>
+<h1 class="hero-title">Garcon</h1>
+<div class="header-center">
+<span>Tasty</span><i class="fa fa-cutlery" aria-hidden="true"></i><span>Delicious</span><i class="fa fa-cutlery" aria-hidden="true"></i><span>Savoury</span>
+</div>
+</div>
+
+
+</section>
+
+<div class="space">
+</div>
+
+<section class="content">
+	
+
+
+<div class="container">
+
+<div class="col-sm-6 img-block">
+<img src="images/collage.jpg" class="img-responsive"/>
+</div>
+
+
+<div class="col-sm-6 special-block text-center">
+<center><p class="top-title"><span>About</span></p></center>
+<center><h2>Our Story</h2></center>
+<p><b>WE HAVE THE GLORY BEGINING IN RESTAURANT BUSINESS</b></p>
+<p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis ullam laboris nisi ut aliquip ex ea commodo consequat.</p>
+</div>
+
+</div>
+
+</section>
+
+<div class="space">
+</div>
+
+<section class="parallax1">
+<div class="inner-warp">
+<div class="container">
+<div class="col-sm-6">
+<img src="images/product-decorate.jpg" class="img-responsive" />
+</div>
+<div class="col-sm-6">
+<h3 class="text-white">Daily Special</h3>
+<hr>
+<div class="content-wrapper">
+<h4 class="text-white">The Country Boy Breakfast<span class="dot">.........................</span>  125</h4>
+<div class="price"></div>
+                                </div>
+
+                                <div class="content-wrapper">
+<h4 class="text-white">The Cracker French fries<span class="dot">.........................</span>  25</h4>
+<div class="price"></div>
+                                </div>
+
+                                <div class="content-wrapper">
+<h4 class="text-white">The Saucy Chicken<span class="dot">.........................</span>  225</h4>
+<div class="price"></div>
+                                </div>
+
+                                         <div class="content-wrapper">
+<h4 class="text-white">The Steak<span class="dot">.........................</span>  325</h4>
+<div class="price"></div>
+                                </div>
+
+
+</div>
+
+</div>
+</div>
+
+</section>
+
+<div class="space">
+</div>
+
+<div class="container">
+
+<center><h3>Tasty And Good Price</h3></center>
+<br>
+<section class="">
+
+<div class="col-sm-6 img-block">
+<img src="images/dessert.png" class="img-responsive" />
+</div>
+
+<div class="col-sm-6">
+
+
+<div class="products">
+                                <div class="item product-01">
+                                  <div class="item-left">
+                                    <h5 class="title">The Country Boy Breakfast</h5>
+                                    <div class="des">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </div>
+                                  </div>
+                                  <div class="item-right"><span class="price woocommerce-Price-amount amount"><span class="price-symbol"></span>25.0</span></div>
+                                </div>
+                                <div class="item product-01">
+                                  <div class="item-left">
+                                    <h5 class="title">Uncle Favorite </h5>
+                                    <div class="des">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </div>
+                                  </div>
+                                  <div class="item-right"><span class="price woocommerce-Price-amount amount"><span class="price-symbol"></span>45.0</span></div>
+                                </div>
+                                <div class="item product-01">
+                                  <div class="item-left">
+                                    <h5 class="title">Fried Breakfast </h5>
+                                    <div class="des">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </div>
+                                  </div>
+                                  <div class="item-right"><span class="price woocommerce-Price-amount amount"><span class="price-symbol"></span>30.0</span></div>
+                                </div>
+                                <div class="item product-01">
+                                  <div class="item-left">
+                                    <h5 class="title">Old Meat Breakfast</h5>
+                                    <div class="des">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </div>
+                                  </div>
+                                  <div class="item-right"><span class="price woocommerce-Price-amount amount"><span class="price-symbol"></span>12.0</span></div>
+                                </div>
+                                <div class="item product-01">
+                                  <div class="item-left">
+                                    <h5 class="title">Chinese Chicken Bread Spicy Soup</h5>
+                                    <div class="des">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </div>
+                                  </div>
+                                  <div class="item-right"><span class="price woocommerce-Price-amount amount"><span class="price-symbol"></span>12.0</span></div>
+                                </div>
+                              </div>
+
+
+
+</div>
+
+</section>
+
+</div>
+
+
+
+<section class="parallax2 testimonials">
+
+
+<div class="inner-warp">
+<div class="container">
+<h3 class="title text-white text-center">Our Customer Says</h3><br><br>
+<div class="testi-content">
+<p class="text-center testicolor">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+
+</div>
+</div>
+
+</div>
+	
+
+
+</section>
+
+
+<section>
+	
+<div class="">
+
+<div class="col-sm-6 reserv-dark">
+<h3 class="text-center text-white">Reservation</h3>
+<br>
+<div class="swin-sc swin-sc-contact-form dark mtl">
+                        <form>
+                          <div class="form-group">
+                            <div class="input-group">
+                              <div class="input-group-addon">
+                                <div class="fa fa-phone"></div>
+                              </div>
+                              <input type="text" placeholder="Phone" class="form-control">
+                            </div>
+                            <div class="input-group">
+                              <div class="input-group-addon"><i class="fa fa-male"></i></div>
+                              <select type="text" placeholder="People" class="form-control">
+                                <option>1 person</option>
+                                <option>2 People</option>
+                                <option>3 People</option>
+                                <option>4 People</option>
+                                <option>5 People</option>
+                                <option>6 People</option>
+                                <option>7 People</option>
+                                <option>8 People</option>
+                                <option>9 People</option>
+                                <option>10 People</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <div class="input-group">
+                              <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                              <input type="text" placeholder="Date" class="form-control datepicker">
+                            </div>
+                            <div class="input-group">
+                              <div class="input-group-addon">
+                                <div class="fa fa-clock-o"></div>
+                              </div>
+                              <select type="text" placeholder="Time" class="form-control">
+                                <option>7:00 AM</option>
+                                <option>8:00 AM</option>
+                                <option>9:00 AM</option>
+                                <option>10:00 AM</option>
+                                <option>11:00 AM</option>
+                                <option>12:00 AM</option>
+                                <option>1:00 PM</option>
+                                <option>2:00 PM</option>
+                                <option>3:00 PM</option>
+                                <option>4:00 PM</option>
+                                <option>5:00 PM</option>
+                                <option>6:00 PM</option>
+                                <option>7:00 PM</option>
+                                <option>8:00 PM</option>
+                                <option>9:00 PM</option>
+                                <option>10:00 PM</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="form-group">
+                          <br>
+                           <center> <div class="swin-btn-wrap center"><a href="#" class="swin-btn center form-submit"> <span>	Book Table</span></a></div></center>
+                          </div>
+                        </form>
+                      </div>
+</div>
+
+<div class="col-sm-6 reser-img">
+<img src="images/reserve.jpg" class="img-responsive"/>
+</div>
+
+</div>
+
+</section>
+
+
+<section class="parallax3 footer">
+
+
+<div class="inner-warp-footer">
+<div class="container">
+
+<div class="col-sm-3">
+<h4 class="text-white text-center">Quick Links</h4>
+</div>
+
+
+<div class="col-sm-3">
+<h4 class="text-white text-center">Contact</h4>
+</div>
+
+
+<div class="col-sm-3">
+<h4 class="text-white text-center">Here We Are</h4>
+</div>
+
+
+</div>
+
+</div>
+	
+
+
+</section>
+
+
+<footer>
+	
+	<?php include 'assets-footer.php'; ?>
+
+</footer>
+
+</body>
+</html>
